57a032c79a44233a4b9cf04b5cf9d155a9461d4c
starigg <starkovivan3@gmail.com>
2022-05-25 12:50:39 +0800

    Рендер пицц, категорий. Spread оператор

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Categories () {
+    const [activeCategory, setActiveCategory] = useState(0);
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые',
+    ]
+
+    const setCategory = (categoryNumber) => {
+        setActiveCategory(categoryNumber);
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    categories.map((item, id) => <li
+                        key={id}
+                        className={activeCategory === id ? `active` : ''}
+                        onClick={() => setCategory(id)}>{item}</li>)
+                }
+
             </ul>
         </div>
     );
